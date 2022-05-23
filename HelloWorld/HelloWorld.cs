7dde5733bd42876a1a8eb219e979b9e939cef166
rossx708 <rossx708@umn.edu>
2022-05-23 19:06:39 -0400

    first c# program in VSCode :)

---
+++ HelloWorld/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HelloWorld
+{
+    class MainProgram
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentTime = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}. It is Present Day {currentTime:d}, at Present Time {currentTime:t}!");
+            Console.WriteLine("Press any key to exit program....");
+            Console.ReadKey(true);
+        }
+    }
+}
